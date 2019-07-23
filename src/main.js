d6a16edcf5838e8d3c34dc5e58e7e7223076bdec
Rustam <punk60826@gmail.com>
2019-07-23 22:19:37 +0300

    react

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 require("babel-runtime/regenerator")
-require("./main.css") 
 require("webpack-hot-middleware/client?reload=true")
+require("react-hot-loader/patch")
+
+require("./main.css") 
 require("./index.html")
 
